f0d140371e5ecb83e84b979a16513794b9903a9c
Brian <brian@jscoder.dev>
2024-04-15 18:53:34 -0500

    initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"; // Import React
+import logo from "./logo.png"; // Assuming the logo.png file is in the same folder as this component
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={logo} className="logo react" alt="React logo" />{" "}
+        <h1>Hunter Evans</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
