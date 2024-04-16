0242e9800d7dd9b9c92dde85397f66b65e6b6143
Brian <brian@jscoder.dev>
2024-04-16 18:29:20 -0500

    updated flexbox components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,33 +3,42 @@ import logo from "./logo.png"; // Assuming the logo.png file is in the same fold
 
 function App() {
   return (
-    <>
-      <div className="flex-container">
-        {/* Navigation Section */}
-        <div
-          className="row"
-          style={{
-            display: "flex",
-            backgroundColor: "black",
-            alignItems: "center",
-            justifyContent: "center",
-            width: "100vw",
-          }}
-        >
-          {/* Navigation Links */}
-          <div style={navLinkStyle}>Home</div>
-          <div style={navLinkStyle}>Services</div>
-          <div style={navLinkStyle}>Contact</div>
-          <div style={navLinkStyle}>Other</div>
-        </div>
-        {/* Main Content Section */}
-        <div className="row">
-          {/* Main Content */}
-          <div style={mainContentStyle}>Hero</div>
-          <div style={outerBoxStyle}>outer box 3</div>
-        </div>
+    <div
+      className="flex-container"
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {/* Navigation Section */}
+      <div
+        className="row"
+        style={{
+          display: "flex",
+          backgroundColor: "black",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100vw",
+        }}
+      >
+        {/* Navigation Links */}
+        <div className="row">Hunter Evans</div>
+        <div style={navLinkStyle}>Home</div>
+        <div style={navLinkStyle}>Services</div>
+        <div style={navLinkStyle}>Contact</div>
+        <div style={navLinkStyle}>Other</div>
       </div>
-    </>
+      {/* Main Content Section */}
+      <div className="row">
+        {/* Main Content */}
+        <div style={mainContentStyle}>Hero</div>
+        <div style={outerBoxStyle}>outer box 3</div>
+        <div style={outerBoxStyle}>outer box 4</div>
+        <div style={outerBoxStyle}>outer box 5</div>
+        <div style={outerBoxStyle}>outer box 6</div>
+      </div>
+    </div>
   );
 }
 
@@ -45,12 +54,16 @@ const navLinkStyle = {
 const mainContentStyle = {
   display: "flex",
   border: "1px solid red",
-  height: "80vh",
+  height: "60vh",
   background: "grey",
+  color: "white",
 };
-
 const outerBoxStyle = {
-  border: "1px solid blue",
+  display: "flex",
+  border: "1px solid red",
+  height: "30vh",
+  background: "blue",
+  color: "white",
 };
 
 export default App;
